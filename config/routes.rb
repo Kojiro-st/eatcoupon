36faa057aacf9b5d969226f12d285311696a9ddf
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-09 20:40:06 +0900

    5th(done controller to top.page)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  resources :customers, only: [:index]
+
   devise_for :users
   # get 'messages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200309062405) do
+ActiveRecord::Schema.define(version: 20200309081000) do
+
+  create_table "customers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
